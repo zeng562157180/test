2a33f71cfc9a45fcb6d74590bce3985172ba04f2
xin.zeng <xin.zeng@fksaas.com>
2019-12-10 19:41:11 +0800

    aaa

---
+++ Test/src/cn/fk/test.java
@@ -0,0 +1,10 @@
+package cn.fk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
